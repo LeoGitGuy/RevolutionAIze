0c42bab65c205046352ba201bc8512b4d35b81ee
LeoGitGuy <leonard.schenk@web.de>
2022-04-22 12:29:07 +0200

    changed theme

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -7,10 +7,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' as rootBundle;
 import 'package:http/http.dart' as http;
 import 'package:t4gopengov/GovData.dart';
-import 'home_screen.dart';
+import 'screens/home_screen.dart';
 
 // Main Function
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // Define the default brightness and colors.
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+
+        // Define the default font family.
+        fontFamily: 'Georgia',
+
+        // Define the default `TextTheme`. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+      ),
       home: HomeScreen(), //we could imagine more sides/ screen
     ));
